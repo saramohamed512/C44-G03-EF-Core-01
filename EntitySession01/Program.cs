24976eb42918e7f294004ffaff1a729f47ba4516
saramohamed512 <saramohamed12656@gmail.com>
2025-09-04 21:32:20 +0200

    EF Core VS Dapper VS ADO.NET

--- EntitySession01/Program.cs
+++ EntitySession01/Program.cs
@@ -6,7 +6,46 @@ namespace EntitySession01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Entity Framework Core
+            /*
+             * Features :-
+             *      - Obj Relational Mapper [ORM] =>Fully ORM
+             *      -Auto Tracker
+             *      -Support Migrations
+             *      -Support LINQ
+             *      -Cross Platform
+             *      -Support Multiple DBs
+             * Disadvantages :-
+             *      -Performance (Auto Tracker)
+             *      -Complexity
+             * 
+             * 
+             */
+            #endregion
+            #region ADO.NET
+            /*
+             * Features :-
+             *      - Low Level Framework
+             *      -No Tracking
+             *      -Manual Connection
+             *      -Direct Access to SQL
+             *     
+             */
+            #endregion
+            #region Dapper
+            /*
+             * Features :-
+             *      - Lightweight for Simple , Small Projects
+             *      -No Tracker
+             *      -No Migrations
+             *      -Simple API (Dapper)
+             *      -Faster
+             *      -Easy to use
+             *      -Micro ORM
+             *          Limited Features
+             *     
+             */
+            #endregion
         }
     }
 }
