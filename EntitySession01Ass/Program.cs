3e81566d490c851d5571e7d7c60ae17384e0ce0c
saramohamed512 <saramohamed12656@gmail.com>
2025-09-04 20:51:55 +0200

    add assignment project

---
+++ EntitySession01Ass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntitySession01Ass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
